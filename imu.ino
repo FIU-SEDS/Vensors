f7b53b588b0594de9389d889d797294c6c13b922
Luis Canada <canadaluis5@gmail.com>
2025-03-12 23:27:00 -0400

    Add sensors base code

---
+++ imu.ino
@@ -0,0 +1,70 @@
+/**
+ * Returns the acceleration from the IMU in the X direction
+ */
+int getIMUXAcc()
+{
+  // Simulate reading from a sensor
+  return -1;
+}
+
+/**
+ * Returns the acceleration from the IUM in the Y direction
+ */
+int getIMUYAcc()
+{
+  // Simulate reading from a sensor
+  return -1;
+}
+
+/**
+ * Returns the acceleration from the in the Z direction
+ */
+int getIMUZAcc()
+{
+  // Simulate reading from a sensor
+  return -1;
+}
+
+/**
+ * Returns the gyro from the IMU in the X direction
+ */
+int getIMUXGyro()
+{
+  // Simulate reading from a sensor
+  return -1;
+}
+
+/**
+ * Returns the gyro from the IMU in the Y direction
+ */
+int getIMUYGyro()
+{
+  // Simulate reading from a sensor
+  return -1;
+}
+
+/**
+ * Returns the gyro from the IMU in the Z direction
+ */
+int getIMUZGyro()
+{
+  // Simulate reading from a sensor
+  return -1;
+}
+
+/**
+ * Returns the temperature from the IMU in Celsius
+ */
+int getIMUZTemp()
+{
+  // Simulate reading from a sensor
+  return -1;
+}
+
+/**
+ * Initalized the IMU and returns a 0 value if successfull.
+ * @return 0 on success, non-zero otherwise.
+ */
+int setupIMU()
+{
+}
---
+++ timeOfFlight.ino
@@ -0,0 +1,25 @@
+/**
+ * Returns the distance measured by the left sensor in mm
+ */
+int getLDistanceSensor()
+{
+  // Simulate reading from a sensor
+  return -1;
+}
+
+/**
+ * Returns the distance measured by the right sensor in mm
+ */
+int getRDistanceSensor()
+{
+  // Simulate reading from a sensor
+  return -1;
+}
+
+/**
+ * Initalized the time of flight sensors and returns a 0 value if successfull.
+ * @return 0 on success, non-zero otherwise.
+ */
+int setupTimeOfFlight()
+{
+}
